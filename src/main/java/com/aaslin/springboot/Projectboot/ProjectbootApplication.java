d9d74784be6bcaa0356bdfc637aa95e4ba26c373
muraliaditya <muraliaditya06@gmail.com>
2025-08-14 17:32:21 +0530

    spring boot basics

---
+++ src/main/java/com/aaslin/springboot/Projectboot/ProjectbootApplication.java
@@ -0,0 +1,13 @@
+package com.aaslin.springboot.Projectboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aaslin/springboot/Projectboot/ProjectbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aaslin.springboot.Projectboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
